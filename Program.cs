7d02a5b0bb16484b3d08602f5a0ea0fe5ebc0716
Poerri <spaideri@protonmail.com>
2021-02-09 18:30:16 +0200

    Testicommitti

--- Program.cs
+++ Program.cs
@@ -21,12 +21,7 @@ namespace JsonPalikka
 
             if (valinta == 1)
                 HenkilöLisuri();
-            if (valinta == 2)
-                Luokittaja();
-            if (valinta == 3)
-                Listoittaja();
-            if (valinta == 4)
-                Listahimmeli();
+
 
         }
 
@@ -74,117 +69,7 @@ namespace JsonPalikka
             Console.ReadKey();
         }
 
-        public static void Luokittaja()
-        {
-            Console.Clear();
-            Console.WriteLine("Jsonista luokaksi");
-
-            string tiedostosta = File.ReadAllText("D:\\temp\\duunaritesti2.json");
-
-            //List<Työntekijä> mestarit = JsonConvert.DeserializeObject<List<Työntekijä>>(tiedostosta);
-
-            //Console.WriteLine(mestarit);
-            Console.WriteLine(tiedostosta);
-
-
-
-
-
-
-            Console.ReadKey();
-        }
-
-        public static void Listoittaja()
-        {
-            Työntekijä duunari = new Työntekijä();
-
-            Console.WriteLine("Anna etunimet: ");
-            duunari.Etunimet = Console.ReadLine();
-
-            Console.Write("Anna sukunimi: ");
-            duunari.Sukunimi = Console.ReadLine();
-
-            Console.Write("Anna henkilönumero: ");
-            duunari.Henkilönumero = int.Parse(Console.ReadLine());
-
-            Console.Write("Syötä palkka: ");
-            duunari.Palkka = int.Parse(Console.ReadLine());
-
-            Console.Write("Anna ammattinimike: ");
-            duunari.Ammattinimike = Console.ReadLine();
-
-            Työntekijä duunari2 = new Työntekijä();
-
-            Console.WriteLine("Anna etunimet: ");
-            duunari2.Etunimet = Console.ReadLine();
-
-            Console.Write("Anna sukunimi: ");
-            duunari2.Sukunimi = Console.ReadLine();
-
-            Console.Write("Anna henkilönumero: ");
-            duunari2.Henkilönumero = int.Parse(Console.ReadLine());
-
-            Console.Write("Syötä palkka: ");
-            duunari2.Palkka = int.Parse(Console.ReadLine());
-
-            Console.Write("Anna ammattinimike: ");
-            duunari2.Ammattinimike = Console.ReadLine();
-
-
-            List<Työntekijä> duunarit = new List<Työntekijä>();
-            duunarit.Add(duunari);
-            duunarit.Add(duunari2);
-
-            string jsonni = JsonConvert.SerializeObject(duunarit);
-
-            using (StreamWriter tiedosto = new StreamWriter("D:\\temp\\listajson.json"))
-            {
-                tiedosto.WriteLine(jsonni);
-            }
-
-
-
-            Console.ReadLine();
-        }
-
-        public static void Listahimmeli()
-        {
-            Console.Clear();
-            Console.WriteLine("Luetaan jsonista");
-
-            string luettuJson = File.ReadAllText("D:\\temp\\listajson.json");
-
-            List<Työntekijä> duunarit = JsonConvert.DeserializeObject<List<Työntekijä>>(luettuJson);
-
-            Työntekijä duunari = new Työntekijä();
-
-            Console.WriteLine("Anna etunimet: ");
-            duunari.Etunimet = Console.ReadLine();
-
-            Console.Write("Anna sukunimi: ");
-            duunari.Sukunimi = Console.ReadLine();
-
-            Console.Write("Anna henkilönumero: ");
-            duunari.Henkilönumero = int.Parse(Console.ReadLine());
-
-            Console.Write("Syötä palkka: ");
-            duunari.Palkka = int.Parse(Console.ReadLine());
-
-            Console.Write("Anna ammattinimike: ");
-            duunari.Ammattinimike = Console.ReadLine();
-
-            duunarit.Add(duunari);
-
-            string luokkaJsoniksi = JsonConvert.SerializeObject(duunarit);
-
-            using(StreamWriter tiedosto = new StreamWriter("d:\\temp\\listajson.json"))
-            {
-                tiedosto.WriteLine(luokkaJsoniksi);
-            }
-
-
-
-            Console.ReadLine();
+        
         }
  
 
